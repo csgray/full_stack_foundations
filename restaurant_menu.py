7d8ce25991bc982f465eccc5ddfbf491ac232608
csgray <csgray@alaska.edu>
2016-03-22 11:18:21 -0800

    Add Create, Update, Delete functionality for restaurants

--- restaurant_menu.py
+++ restaurant_menu.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template, request
-from sqlalchemy import create_engine, and_
+from flask import Flask, render_template, request, flash, url_for, redirect
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Restaurant, MenuItem
 app = Flask(__name__)
@@ -10,6 +10,7 @@ Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
+
 @app.route('/')
 @app.route('/restaurant/')
 def show_restaurant():
@@ -17,28 +18,43 @@ def show_restaurant():
     return render_template('restaurant.html', restaurants=restaurants)
 
 
-@app.route('/restaurant/new/')
+@app.route('/restaurant/new/', methods=['GET', 'POST'])
 def new_restaurant():
     if request.method == 'POST':
-        pass
+        restaurant = Restaurant(name=request.form['name'])
+        session.add(restaurant)
+        session.commit()
+        flash("New restaurant created!")
+        return redirect(url_for('show_restaurant'))
     else:
         return render_template('new_restaurant.html')
 
 
-@app.route('/restaurant/<int:restaurant_id>/edit/')
+@app.route('/restaurant/<int:restaurant_id>/edit/', methods=['GET', 'POST'])
 def edit_restaurant(restaurant_id):
     restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
     if request.method == 'POST':
-        pass
+        restaurant.name = request.form['name']
+        session.add(restaurant)
+        session.commit()
+        flash("Restaurant name updated!")
+        return redirect(url_for('show_restaurant'))
     else:
         return render_template('edit_restaurant.html', restaurant=restaurant)
 
 
-@app.route('/restaurant/<int:restaurant_id>/delete/')
+@app.route('/restaurant/<int:restaurant_id>/delete/', methods=['GET', 'POST'])
 def delete_restaurant(restaurant_id):
     restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
     if request.method == 'POST':
-        pass
+        if request.form['confirmation'] == "Yes":
+            session.delete(restaurant)
+            session.commit()
+            flash("Restaurant deleted!")
+            return redirect(url_for('show_restaurant'))
+        if request.form['confirmation'] == "No":
+            flash("Deletion cancelled!")
+            return redirect(url_for('show_restaurant'))
     else:
         return render_template('delete_restaurant.html', restaurant=restaurant)
 
@@ -51,7 +67,7 @@ def show_menu(restaurant_id):
     return render_template('menu.html', restaurant=restaurant, items=items)
 
 
-@app.route('/restaurant/<int:restaurant_id>/menu/new/')
+@app.route('/restaurant/<int:restaurant_id>/menu/new/', methods=['GET', 'POST'])
 def new_menu_item(restaurant_id):
     restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
     if request.method == 'POST':
@@ -60,7 +76,7 @@ def new_menu_item(restaurant_id):
         return render_template('new_menu_item.html', restaurant=restaurant)
 
 
-@app.route('/restaurant/<int:restaurant_id>/menu/<int:menu_id>/edit/')
+@app.route('/restaurant/<int:restaurant_id>/menu/<int:menu_id>/edit/', methods=['GET', 'POST'])
 def edit_menu_item(restaurant_id, menu_id):
     restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
     item = session.query(MenuItem).filter_by(restaurant_id=restaurant_id).filter_by(id=menu_id).one()
@@ -70,9 +86,10 @@ def edit_menu_item(restaurant_id, menu_id):
         return render_template('edit_menu_item.html', restaurant=restaurant, item=item)
 
 
-@app.route('/restaurant/<int:restaurant_id>/menu/<int:menu_id>/delete/')
+@app.route('/restaurant/<int:restaurant_id>/menu/<int:menu_id>/delete/', methods=['GET', 'POST'])
 def delete_menu_item(restaurant_id, menu_id):
     restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
+    item = session.query(MenuItem).filter_by(restaurant_id=restaurant_id).filter_by(id=menu_id).one()
     if request.method == 'POST':
         pass
     else:
@@ -80,5 +97,6 @@ def delete_menu_item(restaurant_id, menu_id):
 
 
 if __name__ == '__main__':
+    app.secret_key = 'unsecure_key'
     app.debug = True
     app.run(host='0.0.0.0', port=5000)
